b4e5a8b995816847384920729541717b2d143f8e
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-21 23:31:57 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,9 +2,11 @@ local QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateCallback('fubuki:drugrun:getCops', function(_, cb)
 	local amount = 0
     for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
-        if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
+        if v.PlayerData.job.name == "police" and (v.PlayerData.job.onduty or Config.RequireOnDuty == false) then
             amount = amount + 1
         end
     end
     cb(amount)
 end)
+
+
